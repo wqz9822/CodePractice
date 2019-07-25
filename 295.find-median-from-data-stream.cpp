2d628dd9767a62146821d7c5d486fe6a3c173a14
George Wang <wqz9822@gmail.com>
2019-07-24 21:33:21 -0400

    solved 295

--- 295.find-median-from-data-stream.cpp
+++ 295.find-median-from-data-stream.cpp
@@ -9,63 +9,85 @@
  * Hard (34.78%)
  * Total Accepted:    116.9K
  * Total Submissions: 311.5K
- * Testcase Example:  '["MedianFinder","addNum","addNum","findMedian","addNum","findMedian"]\n[[],[1],[2],[],[3],[]]'
+ * Testcase Example:
+ * '["MedianFinder","addNum","addNum","findMedian","addNum","findMedian"]\n[[],[1],[2],[],[3],[]]'
  *
  * Median is the middle value in an ordered integer list. If the size of the
  * list is even, there is no middle value. So the median is the mean of the two
  * middle value.
  * For example,
- * 
+ *
  * [2,3,4], the median is 3
- * 
+ *
  * [2,3], the median is (2 + 3) / 2 = 2.5
- * 
+ *
  * Design a data structure that supports the following two operations:
- * 
- * 
+ *
+ *
  * void addNum(int num) - Add a integer number from the data stream to the data
  * structure.
  * double findMedian() - Return the median of all elements so far.
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * Example:
- * 
- * 
+ *
+ *
  * addNum(1)
  * addNum(2)
  * findMedian() -> 1.5
- * addNum(3) 
+ * addNum(3)
  * findMedian() -> 2
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * Follow up:
- * 
- * 
+ *
+ *
  * If all integer numbers from the stream are between 0 and 100, how would you
  * optimize it?
  * If 99% of all integer numbers from the stream are between 0 and 100, how
  * would you optimize it?
- * 
- * 
+ *
+ *
  */
 class MedianFinder {
 public:
-    /** initialize your data structure here. */
-    MedianFinder() {
-        
+  /** initialize your data structure here. */
+  MedianFinder() {}
+
+  void addNum(int num) {
+    if (lo.empty() || num <= mid) {
+      lo.push(num);
+      // note: push number first, then swap hi/lo, because
+      // new element might be the top
+      if (lo.size() > hi.size() + 1) {
+        hi.push(lo.top());
+        lo.pop();
+      }
+    } else {
+      hi.push(num);
+      if (lo.size() < hi.size()) {
+        lo.push(hi.top());
+        hi.pop();
+      }
     }
-    
-    void addNum(int num) {
-        
+    assert(!lo.empty());
+    if ((lo.size() + hi.size()) % 2 == 0) {
+      mid = hi.empty() ? lo.top() : (lo.top() + hi.top()) / 2.0;
+    } else {
+      mid = lo.top();
     }
-    
-    double findMedian() {
-        
-    }
+  }
+
+  double findMedian() { return mid; }
+
+private:
+  std::priority_queue<int, std::vector<int>, std::less<int>> lo;    // max heap
+  std::priority_queue<int, std::vector<int>, std::greater<int>> hi; // min heap
+  double mid = 0;
 };
 
 /**
