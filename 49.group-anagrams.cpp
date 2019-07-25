c1bc402345e06d68bc39f552ea55bc4fd5fbbf5d
George Wang <wqz9822@gmail.com>
2019-07-25 09:11:31 -0400

    add 49

---
+++ 49.group-anagrams.cpp
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=49 lang=cpp
+ *
+ * [49] Group Anagrams
+ *
+ * https://leetcode.com/problems/group-anagrams/description/
+ *
+ * algorithms
+ * Medium (44.66%)
+ * Total Accepted:    365.1K
+ * Total Submissions: 760.1K
+ * Testcase Example:  '["eat","tea","tan","ate","nat","bat"]'
+ *
+ * Given an array of strings, group anagrams together.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+ * Output:
+ * [
+ * ⁠ ["ate","eat","tea"],
+ * ⁠ ["nat","tan"],
+ * ⁠ ["bat"]
+ * ]
+ * 
+ * Note:
+ * 
+ * 
+ * All inputs will be in lowercase.
+ * The order of your output does not matter.
+ * 
+ * 
+ */
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+    }
+};
