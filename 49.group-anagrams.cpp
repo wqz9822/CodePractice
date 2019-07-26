aa800e2359cb7f15a8faf35cca9515421c09fd1a
George Wang <wqz9822@gmail.com>
2019-07-26 19:53:33 -0400

    solved 49

--- 49.group-anagrams.cpp
+++ 49.group-anagrams.cpp
@@ -12,10 +12,10 @@
  * Testcase Example:  '["eat","tea","tan","ate","nat","bat"]'
  *
  * Given an array of strings, group anagrams together.
- * 
+ *
  * Example:
- * 
- * 
+ *
+ *
  * Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
  * Output:
  * [
@@ -23,18 +23,31 @@
  * ⁠ ["nat","tan"],
  * ⁠ ["bat"]
  * ]
- * 
+ *
  * Note:
- * 
- * 
+ *
+ *
  * All inputs will be in lowercase.
  * The order of your output does not matter.
- * 
- * 
+ *
+ *
  */
 class Solution {
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        
+  vector<vector<string>> groupAnagrams(vector<string> &strs) {
+    vector<vector<string>> res;
+    unordered_map<string, vector<string>> dict;
+    for (const auto &str : strs) {
+      multiset<char> anagram;
+      for (const auto &ch : str) {
+        anagram.insert(ch);
+      }
+      auto newStr = string(anagram.begin(), anagram.end());
+      dict[newStr].push_back(str);
     }
+    for (auto &kv : dict) {
+      res.push_back(std::move(kv.second));
+    }
+    return res;
+  }
 };
