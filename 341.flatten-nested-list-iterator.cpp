0f48d76c50ac5682615e6cc1006db1759da247df
George Wang <wqz9822@gmail.com>
2019-08-02 22:22:37 -0400

    solved 341

--- 341.flatten-nested-list-iterator.cpp
+++ 341.flatten-nested-list-iterator.cpp
@@ -12,64 +12,86 @@
  * Testcase Example:  '[[1,1],2,[1,1]]'
  *
  * Given a nested list of integers, implement an iterator to flatten it.
- * 
+ *
  * Each element is either an integer, or a list -- whose elements may also be
  * integers or other lists.
- * 
+ *
  * Example 1:
- * 
- * 
- * 
+ *
+ *
+ *
  * Input: [[1,1],2,[1,1]]
  * Output: [1,1,2,1,1]
  * Explanation: By calling next repeatedly until hasNext returns
- * false, 
+ * false,
  * the order of elements returned by next should be: [1,1,2,1,1].
- * 
- * 
+ *
+ *
  * Example 2:
- * 
- * 
+ *
+ *
  * Input: [1,[4,[6]]]
  * Output: [1,4,6]
  * Explanation: By calling next repeatedly until hasNext returns
- * false, 
+ * false,
  * the order of elements returned by next should be: [1,4,6].
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  */
 /**
  * // This is the interface that allows for creating nested lists.
  * // You should not implement it, or speculate about its implementation
  * class NestedInteger {
  *   public:
- *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     // Return true if this NestedInteger holds a single integer, rather than
+ * a nested list.
  *     bool isInteger() const;
  *
- *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return the single integer that this NestedInteger holds, if it holds a
+ * single integer
  *     // The result is undefined if this NestedInteger holds a nested list
  *     int getInteger() const;
  *
- *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return the nested list that this NestedInteger holds, if it holds a
+ * nested list
  *     // The result is undefined if this NestedInteger holds a single integer
  *     const vector<NestedInteger> &getList() const;
  * };
  */
 class NestedIterator {
 public:
-    NestedIterator(vector<NestedInteger> &nestedList) {
-        
+  explicit NestedIterator(vector<NestedInteger> &nestedList) {
+    const auto res = flatten(nestedList);
+    for (const auto &item : res) {
+      buf_.push(item);
     }
+  }
 
-    int next() {
-        
-    }
+  int next() {
+    int res = buf_.front();
+    buf_.pop();
+    return res;
+  }
 
-    bool hasNext() {
-        
+  bool hasNext() { return !buf_.empty(); }
+
+private:
+  vector<int> flatten(const vector<NestedInteger> &list) {
+    vector<int> res;
+    for (const auto &item : list) {
+      if (item.isInteger()) {
+        res.push_back(item.getInteger());
+      } else {
+        const auto newList = flatten(item.getList());
+        res.insert(res.end(), newList.begin(), newList.end());
+      }
     }
+    return res;
+  }
+
+  queue<int> buf_;
 };
 
 /**
