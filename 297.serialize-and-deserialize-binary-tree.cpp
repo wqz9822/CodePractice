2581c20dd2972e9dcd80eb49bb570af3f359070f
George Wang <wqz9822@gmail.com>
2019-08-12 22:49:35 -0400

    solved 297

--- 297.serialize-and-deserialize-binary-tree.cpp
+++ 297.serialize-and-deserialize-binary-tree.cpp
@@ -15,33 +15,33 @@
  * sequence of bits so that it can be stored in a file or memory buffer, or
  * transmitted across a network connection link to be reconstructed later in
  * the same or another computer environment.
- * 
+ *
  * Design an algorithm to serialize and deserialize a binary tree. There is no
  * restriction on how your serialization/deserialization algorithm should work.
  * You just need to ensure that a binary tree can be serialized to a string and
  * this string can be deserialized to the original tree structure.
- * 
+ *
  * Example: 
- * 
- * 
+ *
+ *
  * You may serialize the following tree:
- * 
+ *
  * ⁠   1
  * ⁠  / \
  * ⁠ 2   3
  * ⁠    / \
  * ⁠   4   5
- * 
+ *
  * as "[1,2,3,null,null,4,5]"
- * 
- * 
+ *
+ *
  * Clarification: The above format is the same as how LeetCode serializes a
  * binary tree. You do not necessarily need to follow this format, so please be
  * creative and come up with different approaches yourself.
- * 
+ *
  * Note: Do not use class member/global/static variables to store states. Your
  * serialize and deserialize algorithms should be stateless.
- * 
+ *
  */
 /**
  * Definition for a binary tree node.
@@ -54,16 +54,45 @@
  */
 class Codec {
 public:
+  // Encodes a tree to a single string.
+  string serialize(const TreeNode *root) {
+    ostringstream res;
+    serializeHelper(root, res);
+    // cout << "serialized:" << res.str() << endl;
+    return res.str();
+  }
 
-    // Encodes a tree to a single string.
-    string serialize(TreeNode* root) {
-        
-    }
+  // Decodes your encoded data to tree.
+  TreeNode *deserialize(const string &data) {
+    istringstream input{data};
+    return deserializeHelper(input);
+  }
 
-    // Decodes your encoded data to tree.
-    TreeNode* deserialize(string data) {
-        
+private:
+  void serializeHelper(const TreeNode *node, ostringstream &res) {
+    if (!node) {
+      res << "# ";
+      return;
     }
+    res << node->val << ' ';
+    serializeHelper(node->left, res);
+    serializeHelper(node->right, res);
+  }
+
+  TreeNode *deserializeHelper(istringstream &input) {
+    if (input.rdbuf()->in_avail() == 0) {
+      return nullptr;
+    }
+    std::string val;
+    input >> val;
+    if (val == "#") {
+      return nullptr;
+    }
+    TreeNode *node = new TreeNode(std::stoi(val));
+    node->left = deserializeHelper(input);
+    node->right = deserializeHelper(input);
+    return node;
+  }
 };
 
 // Your Codec object will be instantiated and called as such:
