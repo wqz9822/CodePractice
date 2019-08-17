9bf15c54c35623fd9570ab7fd2951993f14cdbc4
George Wang <wqz9822@gmail.com>
2019-08-17 13:58:55 -0400

    add 301

---
+++ 301.remove-invalid-parentheses.cpp
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode id=301 lang=cpp
+ *
+ * [301] Remove Invalid Parentheses
+ *
+ * https://leetcode.com/problems/remove-invalid-parentheses/description/
+ *
+ * algorithms
+ * Hard (39.89%)
+ * Total Accepted:    137.9K
+ * Total Submissions: 345K
+ * Testcase Example:  '"()())()"'
+ *
+ * Remove the minimum number of invalid parentheses in order to make the input
+ * string valid. Return all possible results.
+ * 
+ * Note: The input string may contain letters other than the parentheses ( and
+ * ).
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "()())()"
+ * Output: ["()()()", "(())()"]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "(a)())()"
+ * Output: ["(a)()()", "(a())()"]
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: ")("
+ * Output: [""]
+ * 
+ */
+class Solution {
+public:
+    vector<string> removeInvalidParentheses(string s) {
+        
+    }
+};
