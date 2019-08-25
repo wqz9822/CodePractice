8100c189f5545c0f082ee942e26a89f6997f3b15
George Wang <wqz9822@gmail.com>
2019-08-25 16:33:07 -0400

    solved 211 with trie and dfs

--- 211.add-and-search-word-data-structure-design.cpp
+++ 211.add-and-search-word-data-structure-design.cpp
@@ -9,22 +9,23 @@
  * Medium (31.23%)
  * Total Accepted:    125.2K
  * Total Submissions: 397.9K
- * Testcase Example:  '["WordDictionary","addWord","addWord","addWord","search","search","search","search"]\n[[],["bad"],["dad"],["mad"],["pad"],["bad"],[".ad"],["b.."]]'
+ * Testcase Example:
+ * '["WordDictionary","addWord","addWord","addWord","search","search","search","search"]\n[[],["bad"],["dad"],["mad"],["pad"],["bad"],[".ad"],["b.."]]'
  *
  * Design a data structure that supports the following two operations:
- * 
- * 
+ *
+ *
  * void addWord(word)
  * bool search(word)
- * 
- * 
+ *
+ *
  * search(word) can search a literal word or a regular expression string
  * containing only letters a-z or .. A . means it can represent any one
  * letter.
- * 
+ *
  * Example:
- * 
- * 
+ *
+ *
  * addWord("bad")
  * addWord("dad")
  * addWord("mad")
@@ -32,28 +33,76 @@
  * search("bad") -> true
  * search(".ad") -> true
  * search("b..") -> true
- * 
- * 
+ *
+ *
  * Note:
  * You may assume that all words are consist of lowercase letters a-z.
- * 
+ *
  */
+struct TrieNode {
+  TrieNode() { list.resize(26); }
+  std::vector<std::unique_ptr<TrieNode>> list;
+  bool isWordEnd = false;
+};
+
 class WordDictionary {
 public:
-    /** Initialize your data structure here. */
-    WordDictionary() {
-        
+  /** Initialize your data structure here. */
+  WordDictionary() : root(new TrieNode()) {}
+
+  /** Adds a word into the data structure. */
+  void addWord(const string &word) {
+    if (word.empty()) {
+      return;
     }
-    
-    /** Adds a word into the data structure. */
-    void addWord(string word) {
-        
+    TrieNode *cur = root.get();
+    for (size_t i = 0; i < word.size(); ++i) {
+      if (!cur->list[word[i] - 'a']) {
+        cur->list[word[i] - 'a'] = std::make_unique<TrieNode>();
+      }
+      if (i == word.size() - 1) {
+        cur->isWordEnd = true;
+      }
+      cur = cur->list[word[i] - 'a'].get();
     }
-    
-    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
-    bool search(string word) {
-        
+  }
+
+  /** Returns if the word is in the data structure. A word could contain the dot
+   * character '.' to represent any one letter. */
+  bool search(const string &word) {
+    if (word.empty()) {
+      return true;
     }
+    return searchHelper(root.get(), word, 0);
+  }
+
+private:
+  std::unique_ptr<TrieNode> root;
+
+  bool searchHelper(const TrieNode *cur, const string &word, size_t curPos) {
+    // cout << word << "," << word[curPos] << endl;
+    if (word[curPos] != '.') {
+      const auto next = cur->list[word[curPos] - 'a'].get();
+      if (!next) {
+        return false;
+      }
+      if (curPos == word.size() - 1) {
+        return cur->isWordEnd;
+      }
+      return searchHelper(next, word, curPos + 1);
+    }
+    // wildcard
+    if (curPos == word.size() - 1) {
+      return cur->isWordEnd;
+    }
+    for (const auto &next : cur->list) {
+      if (next.get() && curPos < word.size() - 1 &&
+          searchHelper(next.get(), word, curPos + 1)) {
+        return true;
+      }
+    }
+    return false;
+  }
 };
 
 /**
