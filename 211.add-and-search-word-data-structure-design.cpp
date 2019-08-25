8d67c824176cf43813b03d6fdf5dd1cc8783a40d
George Wang <wqz9822@gmail.com>
2019-08-25 11:03:14 -0400

    add 211

---
+++ 211.add-and-search-word-data-structure-design.cpp
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode id=211 lang=cpp
+ *
+ * [211] Add and Search Word - Data structure design
+ *
+ * https://leetcode.com/problems/add-and-search-word-data-structure-design/description/
+ *
+ * algorithms
+ * Medium (31.23%)
+ * Total Accepted:    125.2K
+ * Total Submissions: 397.9K
+ * Testcase Example:  '["WordDictionary","addWord","addWord","addWord","search","search","search","search"]\n[[],["bad"],["dad"],["mad"],["pad"],["bad"],[".ad"],["b.."]]'
+ *
+ * Design a data structure that supports the following two operations:
+ * 
+ * 
+ * void addWord(word)
+ * bool search(word)
+ * 
+ * 
+ * search(word) can search a literal word or a regular expression string
+ * containing only letters a-z or .. A . means it can represent any one
+ * letter.
+ * 
+ * Example:
+ * 
+ * 
+ * addWord("bad")
+ * addWord("dad")
+ * addWord("mad")
+ * search("pad") -> false
+ * search("bad") -> true
+ * search(".ad") -> true
+ * search("b..") -> true
+ * 
+ * 
+ * Note:
+ * You may assume that all words are consist of lowercase letters a-z.
+ * 
+ */
+class WordDictionary {
+public:
+    /** Initialize your data structure here. */
+    WordDictionary() {
+        
+    }
+    
+    /** Adds a word into the data structure. */
+    void addWord(string word) {
+        
+    }
+    
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    bool search(string word) {
+        
+    }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
