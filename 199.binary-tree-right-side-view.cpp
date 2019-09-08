e7a39f565f3bc6b8c192cddf7084ef1bdeee5a37
George Wang <wqz9822@gmail.com>
2019-09-08 18:58:05 -0400

    solved 199 using level order traversal

--- 199.binary-tree-right-side-view.cpp
+++ 199.binary-tree-right-side-view.cpp
@@ -13,20 +13,20 @@
  *
  * Given a binary tree, imagine yourself standing on the right side of it,
  * return the values of the nodes you can see ordered from top to bottom.
- * 
+ *
  * Example:
- * 
- * 
+ *
+ *
  * Input: [1,2,3,null,5,null,4]
  * Output: [1, 3, 4]
  * Explanation:
- * 
+ *
  * ⁠  1            <---
  * ⁠/   \
  * 2     3         <---
  * ⁠\     \
  * ⁠ 5     4       <---
- * 
+ *
  */
 /**
  * Definition for a binary tree node.
@@ -39,7 +39,33 @@
  */
 class Solution {
 public:
-    vector<int> rightSideView(TreeNode* root) {
-        
+  vector<int> rightSideView(TreeNode *root) {
+    vector<int> res;
+    if (!root) {
+      return res;
     }
+    std::queue<std::pair<int, TreeNode *>> nodeQueue;
+    nodeQueue.push(std::make_pair(0, root));
+    int level = 0;
+    TreeNode *prev = nullptr;
+    while (!nodeQueue.empty()) {
+      auto cur = nodeQueue.front();
+      nodeQueue.pop();
+      if (cur.first > level && prev) {
+        res.push_back(prev->val);
+        level = cur.first;
+      }
+      if (cur.second->left) {
+        nodeQueue.push(std::make_pair(cur.first + 1, cur.second->left));
+      }
+      if (cur.second->right) {
+        nodeQueue.push(std::make_pair(cur.first + 1, cur.second->right));
+      }
+      prev = cur.second;
+    }
+    if (prev) {
+      res.push_back(prev->val);
+    }
+    return res;
+  }
 };
