cd192111e94e8b15228f0a77ecb23e80f0cdfc29
George Wang <wqz9822@gmail.com>
2019-09-10 22:14:44 -0400

    add 253

---
+++ 253.meeting-rooms-ii.cpp
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=253 lang=cpp
+ *
+ * [253] Meeting Rooms II
+ *
+ * https://leetcode.com/problems/meeting-rooms-ii/description/
+ *
+ * algorithms
+ * Medium (43.28%)
+ * Total Accepted:    180.3K
+ * Total Submissions: 414.4K
+ * Testcase Example:  '[[0,30],[5,10],[15,20]]'
+ *
+ * Given an array of meeting time intervals consisting of start and end times
+ * [[s1,e1],[s2,e2],...] (si < ei), find the minimum number of conference rooms
+ * required.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [[0, 30],[5, 10],[15, 20]]
+ * Output: 2
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [[7,10],[2,4]]
+ * Output: 1
+ * 
+ * NOTE: input types have been changed on April 15, 2019. Please reset to
+ * default code definition to get new method signature.
+ * 
+ */
+class Solution {
+public:
+    int minMeetingRooms(vector<vector<int>>& intervals) {
+        
+    }
+};
