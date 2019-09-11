b583fab6dc178456b6a4e91a3a003249f52583c7
George Wang <wqz9822@gmail.com>
2019-09-10 21:45:59 -0400

    solved 11

--- 11.container-with-most-water.cpp
+++ 11.container-with-most-water.cpp
@@ -16,29 +16,59 @@
  * endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together
  * with x-axis forms a container, such that the container contains the most
  * water.
- * 
+ *
  * Note: You may not slant the container and n is at least 2.
- * 
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
+ *
  * The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In
  * this case, the max area of water (blue section) the container can contain is
- * 49. 
- * 
- * 
- * 
+ * 49.
+ *
+ *
+ *
  * Example:
- * 
- * 
+ *
+ *
  * Input: [1,8,6,2,5,4,8,3,7]
  * Output: 49
- * 
+ *
  */
 class Solution {
 public:
-    int maxArea(vector<int>& height) {
-        
+  int maxArea(vector<int> &height) {
+    int res = INT_MIN;
+    int len = height.size();
+    vector<int> leftMax;
+    vector<int> rightMax;
+    int left = INT_MIN;
+    int right = INT_MIN;
+    for (size_t i = 0; i < len; ++i) {
+      if (height[i] > left) {
+        leftMax.push_back(i);
+        left = height[i];
+      }
+      if (height[len - i - 1] > right) {
+        rightMax.push_back(len - i - 1);
+        right = height[len - i - 1];
+      }
     }
+
+    for (size_t i = 0; i < leftMax.size(); ++i) {
+      int curLeft = height[leftMax[i]];
+      for (size_t j = 0; j < rightMax.size(); ++j) {
+        int curRight = height[rightMax[j]];
+        int t = min(curLeft, curRight) * (rightMax[j] - leftMax[i]);
+        if (t > res) {
+          res = t;
+        }
+        if (curLeft < curRight) {
+          break;
+        }
+      }
+    }
+    return res;
+  }
 };
